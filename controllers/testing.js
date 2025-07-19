ec4ae2e10c3eeff19903b0034d8ba5bc5dc577e7
Ilya Lyudevig <lyudevig@gmail.com>
2025-07-19 14:06:48 +0300

    feat: Hash password for test user during database reset

--- controllers/testing.js
+++ controllers/testing.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const Article = require('../models/article');
 
@@ -5,10 +6,13 @@ const reset = async (req, res, next) => {
   try {
     await User.deleteMany({});
     await Article.deleteMany({});
+    
+    const hashedPassword = await bcrypt.hash(process.env.TEST_USER_PASSWORD, 10);
+    
     await User.create({
       name: process.env.TEST_USER_NAME,
       email: process.env.TEST_USER_EMAIL,
-      password: process.env.TEST_USER_PASSWORD,
+      password: hashedPassword,
     });
     res.status(204).send();
   } catch (err) {
